1d979a6a3b0d3e623c0e42e53c04fb7a14c41458
rajneesh.mishra@encapto.com <rajneesh.mishra@encapto.com>
2019-08-10 19:56:27 +0530

    read users

--- src/index.js
+++ src/index.js
@@ -19,6 +19,33 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+  User.find()
+    .then(users => {
+      if (!users) {
+        return res.status(404).send();
+      }
+      res.send(users);
+    })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+});
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task
